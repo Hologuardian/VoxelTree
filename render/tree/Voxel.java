632b02d2f3d7917b3d13c18b2cbc5973695ff483
Hologuardian <namaeskram@gmail.com>
2014-04-28 16:59:06 -0400

    Pushing voxel tree code

---
+++ render/tree/Voxel.java
@@ -0,0 +1,5 @@
+package render.tree;
+
+public class Voxel {
+
+}
---
+++ render/tree/VoxelTree.java
@@ -0,0 +1,13 @@
+package render.tree;
+
+import java.util.ArrayList;
+
+public class VoxelTree 
+{
+	public VoxelTree[] voxelList = new VoxelTree[8];
+	
+	public VoxelTree()
+	{
+		
+	}
+}
