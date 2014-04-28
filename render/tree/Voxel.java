e2c7b84de075b9167369db43a62ebed851b41b4c
Hologuardian <namaeskram@gmail.com>
2014-04-28 17:14:48 -0400

    Made the tree and added some fields for the voxel class.

--- render/tree/Voxel.java
+++ render/tree/Voxel.java
@@ -1,5 +1,16 @@
 package render.tree;
 
-public class Voxel {
-
+public class Voxel 
+{
+	//The coordinates of the lowest corner of the voxel;
+	public float x;
+	public float y;
+	
+	//The width and height of the voxel
+	public float width;
+	public float height;
+	
+	//Since the tree can only store size 1/(n^x) voxels using a size class for x is a possible way to define voxel size
+	
+	public int sizeClass; 
 }
--- render/tree/VoxelTree.java
+++ render/tree/VoxelTree.java
@@ -1,12 +1,15 @@
 package render.tree;
 
-import java.util.ArrayList;
+
+//Each tree holds a single voxel, and a list of trees that are 8 sub-voxels of the voxel in the tree
+//Tree can be of infinite length allowing any size 1/(n^2) of voxel to be stored and the size of the entire tree is ~n.
 
 public class VoxelTree 
 {
-	public VoxelTree[] voxelList = new VoxelTree[8];
+	public VoxelTree[] voxelList = new VoxelTree[8];// (0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 0), (0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 1)
+	public Voxel slot;
 	
-	public VoxelTree()
+	public VoxelTree(Voxel voxel)
 	{
 		
 	}
